b38a5ad3f992cb5ca6c3e2c4a3ea30417b958e3e
J Mac <joelmmackenzie@gmail.com>
2024-11-29 16:57:16 +1000

    Scaffolding for the driver

--- rbstar/__main__.py
+++ rbstar/__main__.py
@@ -2,16 +2,28 @@ import sys
 import argparse
 
 
-def check_metric(metric: str) -> any: # XXX should be an enum or something probably
-    pass
+def check_metric(metric: str) -> str: # XXX should be an enum or something probably
+    if metric.upper() == 'RBP':
+        return  'RBP'
+    elif metric.upper() == 'RBO':
+        return 'RBO'
+    elif metric.upper() == 'RBA':
+        return 'RBA'
+    elif metric.upper() == 'RBR':
+        return 'RBR'
+    raise ValueError("Provided metric not recognized.")
+
 
 def rbstar_main():
     parser = argparse.ArgumentParser(description="RBStar CLI")
-    parser.add_argument("-m, --metric", type=str, help="Specify the desired metric.")
-    parser.add_argument("--observation", type=str, help="Path to the observation.")
-    parser.add_argument("--reference", type=str, help="Path to the reference.")
-    parser.add_argument("-p, --phi", type=float, default="0.95", help="Persistence parameter.")
+    parser.add_argument("-m", "--metric", type=str, help="Specify the desired metric.", required=True)
+    parser.add_argument("--observation", type=str, help="Path to the observation.", required=True)
+    parser.add_argument("--reference", type=str, help="Path to the reference.", required=True)
+    parser.add_argument("-p", "--phi", type=float, default="0.95", help="Persistence parameter.")
     args = parser.parse_args() 
+    
+    metric_string = check_metric(args.metric)
+    print("Computing", metric_string, "𝜙 =", args.phi)
 
 
 if __name__ == "__main__":
